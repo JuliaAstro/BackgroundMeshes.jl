4cd479d70271d3f2521be1dd5ddbab5670d45d7a
icweaver <weaveric@gmail.com>
2025-10-24 02:22:33 -0700

    import --> using

--- test/background.jl
+++ test/background.jl
@@ -1,8 +1,13 @@
-import BackgroundMeshes:
-    MMMBackground, BiweightLocationBackground, SourceExtractorBackground,
-    StdRMS, MADStdRMS, BiweightScaleRMS,
+using BackgroundMeshes:
+    MMMBackground,
+    BiweightLocationBackground,
+    SourceExtractorBackground,
+    StdRMS,
+    MADStdRMS,
+    BiweightScaleRMS,
     ZoomInterpolator,
-    sigma_clip, estimate_background
+    sigma_clip,
+    estimate_background
 
 @testset "sigma clipping" begin
     x = [1, 2, 3]
@@ -16,9 +21,14 @@ import BackgroundMeshes:
     @test sigma_clip(y, 1; center=4) ≈ [2.1291713066130296, 2.1291713066130296, 3, 4, 5, 5.87082869338697]
     # using different std
     @test sigma_clip(y, 1; std=1) ≈ [2.5, 2.5, 3, 4, 4.5, 4.5]
+    @test count(isnan, sigma_clip(y, 1; fill=NaN)) == 2
+    @test count(isnan, sigma_clip(y, 1; fill=NaN)) == 2
 
-    @test count(isnan, sigma_clip(y, 1; fill=NaN)) == 2
-    @test count(isnan, sigma_clip(y, 1; fill=NaN)) == 2
+    # `sigma_clip` should not mutate
+    z1 = [0.1, 0.2, 3.0, 4.0, 0.2, 0.1]
+    z2 = copy(z1)
+    sigma_clip(z1, 1)
+    @test z1 == z2
 end
 
 @testset "estimate_background interface" begin
